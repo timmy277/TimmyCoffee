ae2f11fad96950efded3be57f597a748702a0dab
xiaokk277 <botono123@gmail.com>
2024-01-03 22:59:28 +0700

    add product cart feature mugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,10 +18,15 @@ module.exports = {
         slideDown: {
           '0%': { transform: 'translateY(-100%)' },
           '100%': { transform: 'translateY(0%)' },
+        },
+        fadeIn: {
+          from: { opacity: 0},
+          to : { opacity: 1}
         }
       },
       animation: {
         slideDown: 'slideDown 0.7s ease-in-out',
+        fadeIn: 'fadeIn 0.5s ease-in-out',
       },
       backgroundImage: {
         'slider-bg': 'url("./image/bgimg.jpg")'
